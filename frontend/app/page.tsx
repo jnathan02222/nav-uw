7cf4c1d4b2c28ba19309e5e36b007510e1185d52
Nathan <test@gmail.com>
2024-12-22 00:24:36 -0500

    Some changes

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,29 +1,81 @@
 'use client'
-import Map, {Source, Marker} from 'react-map-gl';
-import 'mapbox-gl/dist/mapbox-gl.css';
+import { useEffect, useRef } from 'react'
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
 
-export default function Home() {
-  return (
-    <div className="flex h-screen w-full ">
-      <Map
-        mapboxAccessToken="pk.eyJ1IjoibjI1amlhbmciLCJhIjoiY200dm13OGwwMDRkYjJqcG83bnF3dWtycCJ9.qo_qnQ73tFjZTHwLoAjNRA"
-        initialViewState={{
-          latitude: 43.471054068640456,
-          longitude: -80.54273214750569,
-          zoom: 15.5
-        }}
-        
-        mapStyle="mapbox://styles/n25jiang/cm4vz63tg006v01s2gj252vps"
-        attributionControl={false}
-      >
-        <Marker longitude={-80.54390849514651} latitude={43.472702732652365} color="red" />
-        <Marker longitude={-80.54331115845638} latitude={43.47191340742358} color="red" />
-        <Marker longitude={-80.54403467054422} latitude={43.47157147919427} color="red" />
-        <Marker longitude={-80.5446144385124} latitude={43.47240369435188} color="red" />
-        <Source id="MC_01" type="image" url="/_017MC_01FLR.png" coordinates={[[-80.54390849514651, 43.472702732652365],[-80.54331115845638, 43.47191340742358],[-80.54403467054422, 43.47157147919427],[-80.5446144385124, 43.47240369435188]]}></Source>
-      </Map>
-    </div>
-  );
+const MapboxExample = () => {
+  //Constants
+  const images = ["_017MC_01FLR"]
+  const homeCoord : [number, number] = [-80.54273214750569, 43.471054068640456]
+
+  //Refs
+  const mapContainerRef  = useRef<HTMLDivElement>(null)
+  const mapRef = useRef<any>(null)
+  
+  useEffect(() => {
+    if(mapContainerRef.current == null){
+      return
+    }
+    
+    //Create map
+    mapboxgl.accessToken = 'pk.eyJ1IjoibjI1amlhbmciLCJhIjoiY200dm13OGwwMDRkYjJqcG83bnF3dWtycCJ9.qo_qnQ73tFjZTHwLoAjNRA'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      maxZoom: 20,
+      minZoom: 15.5,
+      zoom: 15.5,
+      center: homeCoord,
+      style: 'mapbox://styles/n25jiang/cm4vz63tg006v01s2gj252vps',
+      attributionControl: false
+    })
+
+
+    //Add markers and handlers
+    images.forEach((img)=>{
+      const marker = new mapboxgl.Marker({
+        draggable: true
+      })
+        .setLngLat(homeCoord)
+        .addTo(mapRef.current)
+
+      function onDragEnd() {
+        const lngLat = marker.getLngLat()
+        console.log(lngLat)
+        mapRef.current.getSource(img).updateImage({
+          coordinates: [[lngLat.lng,lngLat.lat],[-80.54326904638356,43.47185045491588],[-80.54405495562503,43.47155847346985],[-80.54458556318968,43.472402900479494]]
+        });
+      }
+      marker.on('dragend', onDragEnd)
+    })
+    
+    //Add overlays
+    mapRef.current.on('load', () => {
+      images.forEach((img)=>{
+        mapRef.current.addSource(img, {
+          type: 'image',
+          url: `/${img}.png`,
+          coordinates: [homeCoord,[-80.54326904638356,43.47185045491588],[-80.54405495562503,43.47155847346985],[-80.54458556318968,43.472402900479494]]
+        })
+        mapRef.current.addLayer({
+          id: `${img}-layer`,
+          type: 'raster',
+          source: img,
+          paint: {
+            'raster-fade-duration': 0
+          }
+        })
+      })
+    })
+
+    //Cleanup
+    return () => {
+      mapRef.current.remove()
+    }
+
+  }, [])
+
+  return <div id="map" ref={mapContainerRef} className='w-full h-screen'></div>
 }
 
+export default MapboxExample
 
